363ae84d8509887419abc58b085d30c965773624
SandeepHooda <sonu.hooda@gmail.com>
2017-04-07 18:21:33 +0530

    using sw-precache

--- WebContent/app/js/main.js
+++ WebContent/app/js/main.js
@@ -3,13 +3,18 @@
 // Register Service Worker
 
 if('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
-    // Registration was successful
-  }).catch(function(err) {
-    // registration failed :(
-    console.log('ServiceWorker registration failed: ', err);
-  });
+	window.addEventListener('load', function() {
+	  navigator.serviceWorker.register('service-worker.js').then(function(registration) {
+	    // Registration was successful
+	  }).catch(function(err) {
+	    // registration failed :(
+	    console.log('ServiceWorker registration failed: ', err);
+	  });
+	});
 }
+
+
+	
 
 // Setup Push notifications
 
